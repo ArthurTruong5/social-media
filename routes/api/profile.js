7d6e2e21bfe1d789b77406261e4e51591f3951cc
arthurtruong <arthurtruong.work@gmail.com>
2018-09-14 02:33:11 +1000

    profile route and validation

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -21,21 +21,61 @@ const User = require('../../models/User');
 router.get('/test', (req,res) => res.json({msg: "Profile Works"}));
 
 // @route GET api/profile/
-// @desc - Get current users info
+// @desc - Create or edit user profile
 // @access, Private
-router.get('/', passport.authenticate('jwt', { session: false }), (req,res) => {
-  // initialize errors and put all errors into an object
-  const errors = {}
-  // Gives promise
-  Profile.findOne({ user: req.user.id })
-  .then(profile => {
-    if(!profile) {
-      errors.noprofile = "No profile for this user";
-      return res.status(404).json(errors);
-    }
-    res.json(profile);
-  })
-  .catch(err => res.status(404).json(err));
-});
+// How the below code Works
+// We're going to get everything from the req.body and fill the profileFields inside the object including the social object.
+router.post(
+  '/',
+   passport.authenticate('jwt', { session: false }), (req,res) => {
+     const profileFields = {};
+     profileFields.user = req.user.id;
+     if(req.body.handle) profileFields.handle = req.body.handle;
+     if(req.body.company) profileFields.company = req.body.company;
+     if(req.body.website) profileFields.website = req.body.website;
+     if(req.body.location) profileFields.location = req.body.location;
+     if(req.body.bio) profileFields.bio = req.body.bio;
+     if(req.body.status) profileFields.status = req.body.status;
+     if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+     // SKills - Split into array
+     if(typeof req.body.skills !== 'undefined') {
+       profileFields.skills = req.body.skills.split(',');
+     }
+     // Social
+     // If we try to add profile fields it will say profilefields.social doesn't exist
+     profileFields.social = {};
+     if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
+     if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+     if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+     if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+     if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+     // We're searching the user for the login user ID. If they have a profile, we're going uo update it
+     Profile.findOne({ user: req.user.id }).then(profile => {
+       if (profile) {
+         // Update
+         Profile.findOneAndUpdate(
+           { user: req.user.id },
+           { $set: profileFields },
+           { new: true }
+         ).then(profile => res.json(profile));
+       } else {
+         // Create
+
+         // Check if handle exists
+         Profile.findOne({ handle: profileFields.handle }).then(profile => {
+           if (profile) {
+             errors.handle = 'That handle already exists';
+             res.status(400).json(errors);
+           }
+
+           // Save Profile
+           new Profile(profileFields).save().then(profile => res.json(profile));
+         });
+       }
+     });
+   }
+ );
+
 
 module.exports = router;
