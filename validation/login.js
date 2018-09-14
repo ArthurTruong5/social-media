657f5f11d63afc597d2d90664d7cbb7015e8cca4
arthurtruong <arthurtruong.work@gmail.com>
2018-09-14 22:37:10 +1000

    profile api routes

--- validation/login.js
+++ validation/login.js
@@ -6,17 +6,12 @@ module.exports = function validateLoginInput(data) {
   // define errors and stores it inside
   let errors = {};
   // (Ternary) If its not empty THEN data.name but if it IS empty then just be an empty string
-  data.name = !isEmpty(data.name) ? data.name : '';
+
   data.email =!isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
+
   console.log(data.email);
 
-  if(!Validator.isLength(data.name,{ min: 2, max: 30 })){
-    errors.name = "Name must be between 2 and 30 characters";
-  }
-  // The ternary converted it to a empty string and gets moved to here. Validator will see an empty string and output an error. We can then display the error name
-  if (Validator.isEmpty(data.name)) {
-    errors.name = "Name field is required";
-  }
 
   if (!Validator.isEmail(data.email)) {
     errors.email = "Invalid Email";
@@ -24,6 +19,10 @@ module.exports = function validateLoginInput(data) {
 
   if (Validator.isEmpty(data.email)) {
     errors.email = "Blank Input! Must put a email";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Blank password, must put a password"
   }
 
   return {
