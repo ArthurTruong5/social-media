c9192b0142ede1ccea40f8563c73a8e649f61e4b
arthurtruong <arthurtruong.work@gmail.com>
2018-09-17 16:57:07 +1000

    react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+// Mimick a standard server
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// Components
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+// Css
 import './App.css';
+
+// @@code <Route exact path= "/" component = { Landing } />
+// @@desc Path = /. Landing is coming from the landing component. We put exact because it shows content from multiple routes
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className= "App">
-        <h1>My React App</h1>
+        <Navbar />
+        <Route exact path= "/" component = {Landing} />
+        <div className ="container">
+          <Route exact path="/register" component={ Register } />
+          <Route exact path="/login" component={Login } />
+
+        </div>
+        <Footer />
       </div>
+      </Router>
     );
   }
 }
