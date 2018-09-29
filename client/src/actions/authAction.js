457ce152e52cfab2b226d7c80ae0c415323dc49e
arthurtruong <arthurtruong.work@gmail.com>
2018-09-29 15:19:56 +1000

    login and set current user

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -5,9 +5,12 @@
 // This is where the thunk middleware comes in. We need dispatch arrow
 
 import axios from 'axios';
+import setAuthToken from '../utils/setAuthToken';
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import jwt_decode from 'jwt-decode';
 
-import { GET_ERRORS } from './types';
 
+// Register User
 export const registerUser = (userData, history) => dispatch => {
 
   axios.post('/api/users/register', userData)
@@ -20,3 +23,35 @@ export const registerUser = (userData, history) => dispatch => {
     })
   );
 };
+
+// Login - Get User Token
+export const loginUser = userData => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      // Save to localStorage
+      const { token } = res.data;
+      // Set token to ls
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  }
+}
---
+++ client/src/validation/is-empty.js
@@ -0,0 +1,8 @@
+// If its undefined, null it will turn it into a empty string for validator to work.
+const isEmpty = value =>
+  value === undefined ||
+  value === null ||
+  (typeof value === 'object' && Object.keys(value).length === 0) ||
+  (typeof value === 'string' && value.trim().length === 0);
+
+export default isEmpty;
