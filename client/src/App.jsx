901b5ff2a413daaa0e575ad261c5cee7cd7f2b8d
arthurtruong <arthurtruong.work@gmail.com>
2018-10-13 02:21:11 +1100

    add experience

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 
 // Css
@@ -77,6 +78,9 @@ class App extends Component {
             </Switch>
             <Switch>
             <PrivateRoute exact path="/edit-profile" component={EditProfile } />
+            </Switch>
+            <Switch>
+            <PrivateRoute exact path="/add-experience" component={AddExperience } />
             </Switch>
           </div>
           <Footer />
--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -73,6 +73,19 @@ export const deleteAccount = () => dispatch => {
   }
 }
 
+// Add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post('/api/profile/experience', expData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 
 
 // Profile loading
